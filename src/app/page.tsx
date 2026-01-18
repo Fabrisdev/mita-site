1ac7679dd94c91cf129d5b365414228939ec540e
fabrisdev <ezepuntocom@hotmail.com>
2026-01-18 04:09:17 -0300

    new background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { AllowedPeople } from "@/types";
 import { ProfileOption } from "./components/ProfileOption";
 
 export default function Home() {
   const [selected, setSelected] = useState<AllowedPeople>();
 
+  useEffect(() => {
+    const bg = new Audio("/bg-music.mp3");
+    bg.loop = true;
+    bg.play();
+  }, []);
+
   function select(person: AllowedPeople) {
     setSelected(person);
   }
 
   return (
-    <main className="h-svh flex justify-center items-center">
+    <main className="h-svh flex justify-center items-center bg-cover">
+      <video
+        autoPlay
+        muted
+        loop
+        src="/mita_stand.mp4"
+        className="fixed"
+      ></video>
       <form
         action=""
-        className="bg-purple-800 p-20 flex flex-col justify-center items-center gap-3 rounded-2xl"
+        className="bg-purple-800 p-10 flex flex-col justify-center items-center gap-3 rounded-2xl fixed left-30"
       >
         <p className="text-2xl font-bold">Please choose who you are</p>
-        <div className="flex gap-2">
+        <div className="flex flex-col gap-2">
           <ProfileOption
             name="light"
             onClick={() => select("light")}
