47c54fcbb717e6bd9ba111b1a29d36da6e6bfa85
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 13:39:44 -0300

    new logout button

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -5,6 +5,8 @@ import { MitaBotLogo } from "@/icons/MitaBotLogo";
 import { MitaBackground } from "../../backgrounds/MitaBackground";
 import { LoggedInAsPopup } from "./LoggedInAsPopup";
 import { LogInButton } from "./LogInButton";
+import { LogoutButton } from "./LogoutButton";
+import { MainMenu } from "./MainMenu";
 
 type Props =
   | {
@@ -23,11 +25,6 @@ type Props =
 export function Home({ initialLoggedIn, avatar, id, username }: Props) {
   const [loggedIn, setLoggedIn] = useState(initialLoggedIn);
 
-  async function handleLogout() {
-    await fetch("/api/logout", { method: "POST" });
-    setLoggedIn(false);
-  }
-
   return (
     <main className="h-svh flex justify-center items-center overflow-hidden">
       {initialLoggedIn && loggedIn && (
@@ -35,28 +32,11 @@ export function Home({ initialLoggedIn, avatar, id, username }: Props) {
       )}
 
       <MitaBackground />
-      <div
-        className="fixed -left-125 animation-delay-[5s] z-1000 bg-purple-600 p-10 rounded-xl flex flex-col gap-7 bg-linear-to-r from-purple-600 via-pink-500 to-blue-500 bg-size-[400%_400%] border-2 border-white"
-        style={{
-          animation: `
-            custom-slide-in-left 1s ease-out 5s forwards,
-            bg-shift 6s linear 6s infinite
-          `,
-          animationDelay: "5s",
-        }}
-      >
+      <MainMenu>
         <MitaBotLogo />
         <LogInButton loggedIn={loggedIn} />
-        {loggedIn && (
-          <button
-            type="button"
-            className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition duration-1500 min-w-60 border-2 border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[7s] animate-duration-3000 font-bold cursor-pointer"
-            onClick={handleLogout}
-          >
-            Log out
-          </button>
-        )}
-      </div>
+        <LogoutButton loggedIn={loggedIn} uiLogout={() => setLoggedIn(false)} />
+      </MainMenu>
     </main>
   );
 }
---
+++ src/app/components/LogoutButton.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  loggedIn: boolean;
+  uiLogout: () => void;
+};
+
+export function LogoutButton({ loggedIn, uiLogout }: Props) {
+  async function handleLogout() {
+    await fetch("/api/logout", { method: "POST" });
+    uiLogout();
+  }
+
+  if (!loggedIn) return null;
+  return (
+    <button
+      type="button"
+      className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition duration-1500 min-w-60 border-2 border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[7s] animate-duration-3000 font-bold cursor-pointer"
+      onClick={handleLogout}
+    >
+      Log out
+    </button>
+  );
+}
