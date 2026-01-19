defd721c53e436812ab6b695c6f4164abb6bfabb
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 01:46:52 -0300

    added discord logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
@@ -99,9 +100,16 @@ export default function Home() {
       >
         <a
           href="https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify"
-          className={`bg-purple-800 p-2 flex flex-col justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition min-w-60 border border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000`}
+          className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition min-w-60 border border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000 relative"
         >
-          Log in with Discord
+          <Image
+            src="/discord.svg"
+            alt="Discord logo"
+            width={40}
+            height={40}
+            className="absolute -top-5 -left-5 -rotate-30"
+          />
+          <p className="font-bold">Log in with Discord</p>
         </a>
       </div>
     </main>
