9f979c1c120c292faa981f7ad20333b458de1566
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 16:59:01 -0300

    changing href on production

--- src/auth/components/LogInButton.tsx
+++ src/auth/components/LogInButton.tsx
@@ -5,12 +5,18 @@ type Props = {
 };
 
 export function LogInButton({ loggedIn }: Props) {
+  const PRODUCTION_HREF =
+    "https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=https%3A%2F%2Fmita-bot.fabris.dev&scope=identify";
+  const DEVELOPMENT_HREF =
+    "https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify";
   return (
     <a
       href={
         loggedIn
           ? "/panel"
-          : "https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify"
+          : process.env.NODE_ENV === "production"
+            ? PRODUCTION_HREF
+            : DEVELOPMENT_HREF
       }
       className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition duration-1500 min-w-60 border-2 border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000 relative"
     >
