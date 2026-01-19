deae1a8788b1cc77c373c5d6f6dc6bd306611ed0
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 03:42:44 -0300

    correct link when logged in

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
 type Props =
@@ -129,7 +128,11 @@ export function Home({ initialLoggedIn, avatar }: Props) {
           height={77}
         />
         <a
-          href="https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify"
+          href={
+            loggedIn
+              ? "/panel"
+              : "https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify"
+          }
           className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition duration-1500 min-w-60 border-2 border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000 relative"
         >
           <Image
