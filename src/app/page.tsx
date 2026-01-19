ede4d655301d458baa232f2bd71b74103982ed40
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 02:16:48 -0300

    Better styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,9 +92,12 @@ export default function Home() {
         }}
       />
       <div
-        className="fixed -left-125 animation-delay-[5s] z-1000 bg-purple-600 p-10 rounded-xl flex flex-col gap-7"
+        className="fixed -left-125 animation-delay-[5s] z-1000 bg-purple-600 p-10 rounded-xl flex flex-col gap-7 bg-linear-to-r from-purple-600 via-pink-500 to-blue-500 bg-size-[400%_400%] border-2 border-white"
         style={{
-          animation: "custom-slide-in-left 1s ease-out forwards",
+          animation: `
+            custom-slide-in-left 1s ease-out 5s forwards,
+            bg-shift 6s linear 6s infinite
+          `,
           animationDelay: "5s",
         }}
       >
@@ -106,7 +109,7 @@ export default function Home() {
         />
         <a
           href="https://discord.com/oauth2/authorize?client_id=1461518757015851111&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fverify&scope=identify"
-          className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition min-w-60 border border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000 relative"
+          className="bg-purple-800 p-2 flex justify-center items-center gap-3 rounded-xs hover:bg-pink-500 transition duration-1500 min-w-60 border-2 border-pink-500 animate-heartbeat animate-iteration-count-infinite animate-delay-[6s] animate-duration-3000 relative"
         >
           <Image
             src="/discord.svg"
