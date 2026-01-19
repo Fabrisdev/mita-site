9288e5624dfe5a1720e9104ea8d8721cdea159de
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 15:19:05 -0300

    working on admin panel

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,3 +1,24 @@
-export default function AdminPanelPage() {
-  return <h1>Welcome to the Admin panel</h1>;
+import { jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { LoggedInAsPopup } from "@/auth/components/LoggedInAsPopup";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export default async function AdminPanelPage() {
+  const cookieStore = await cookies();
+  const session = cookieStore.get("session");
+  if (!session) return redirect("/");
+  const verifyResult = await jwtVerify(session.value, secret);
+  const { payload } = verifyResult;
+  const { avatar, id, username } = payload;
+  return (
+    <div>
+      <LoggedInAsPopup
+        avatar={avatar as string}
+        id={id as string}
+        username={username as string}
+      />
+    </div>
+  );
 }
