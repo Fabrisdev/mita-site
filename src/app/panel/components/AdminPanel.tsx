4c939d72642ce98e0cb567fa9980545c4d5ee867
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 15:26:59 -0300

    Added simple send message form

--- src/app/panel/components/AdminPanel.tsx
+++ src/app/panel/components/AdminPanel.tsx
@@ -10,6 +10,16 @@ export function AdminPanel({ username, id, avatar }: Props) {
   return (
     <div>
       <LoggedInAsPopup username={username} id={id} avatar={avatar} />
+      <form
+        action="/api/admin/send-message"
+        method="POST"
+        className="bg-gray-500 flex flex-col w-min p-2"
+      >
+        <input type="text" name="message" placeholder="Message" />
+        <input type="text" name="guildId" placeholder="Guild ID" />
+        <input type="text" name="channelId" placeholder="Channel ID" />
+        <input type="submit" value="Send" />
+      </form>
     </div>
   );
 }
