f24101fa1b7da2f9761ab41295aab3ebfcaa352b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-19 00:27:37 -0300

    Added middleware for checking session

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { jwtVerify } from "jose";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function middleware(req: NextRequest) {
+  const session = req.cookies.get("session");
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  try {
+    await jwtVerify(session.value, secret);
+    return NextResponse.next();
+  } catch {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/panel/:path*"],
+};
