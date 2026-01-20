6e47358422c8c93e73abe1113ddf24c09f02a42b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-20 07:31:24 -0300

    Remove no longer existing animation

--- src/app/components/MainMenu.tsx
+++ src/app/components/MainMenu.tsx
@@ -1,15 +1,6 @@
 export function MainMenu({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className="fixed -left-125 animation-delay-[5s] z-1000 bg-purple-600 p-10 rounded-xl flex flex-col gap-7 bg-linear-to-r from-purple-600 via-pink-500 to-blue-500 bg-size-[400%_400%] border-2 border-white"
-      style={{
-        animation: `
-            custom-slide-in-left 1s ease-out 5s forwards,
-            bg-shift 6s linear 6s infinite
-          `,
-        animationDelay: "5s",
-      }}
-    >
+    <div className="fixed -left-125 animation-delay-[5s] z-1000 bg-purple-600 p-10 rounded-xl flex flex-col gap-7 bg-linear-to-r from-purple-600 via-pink-500 to-blue-500 bg-size-[400%_400%] border-2 border-white animate-[custom-slide-in-left_1s_ease-out_5s_forwards] animate-delay-5000">
       {children}
     </div>
   );
