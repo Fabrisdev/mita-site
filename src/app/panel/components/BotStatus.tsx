8aa5a926eadb255cf7d98563f455882778b8e5cb
fabrisdev <ezepuntocom@hotmail.com>
2026-01-22 21:49:15 -0300

    reworked bot status

--- src/app/panel/components/BotStatus.tsx
+++ src/app/panel/components/BotStatus.tsx
@@ -1,7 +1,17 @@
-import { GET as checkOk } from "@/app/api/admin/ok/route";
+"use client";
 
-export async function BotStatus() {
-  const isOK = await checkOk().then((res) => res.ok);
+import { useEffect, useState } from "react";
+import { BOT_API_URL } from "@/consts";
+
+export function BotStatus() {
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    fetch(`${BOT_API_URL}/status/ok`)
+      .then(() => setIsOnline(true))
+      .catch(() => setIsOnline(false));
+  }, []);
+
   const formattedTime = new Date().toLocaleTimeString("en-US", {
     hour12: true,
     hour: "numeric",
@@ -11,9 +21,9 @@ export async function BotStatus() {
   return (
     <div className="flex justify-center items-center gap-2">
       <span
-        className={`font-bold p-2 rounded-xl border-2 ${isOK ? "bg-green-400" : "bg-red-400"}`}
+        className={`font-bold p-2 rounded-xl border-2 ${isOnline ? "bg-green-400" : "bg-red-400"}`}
       >
-        {isOK ? "ONLINE" : "OFFLINE"}
+        {isOnline ? "ONLINE" : "OFFLINE"}
       </span>
       <i className="text-">(last checked at: {formattedTime})</i>
     </div>
