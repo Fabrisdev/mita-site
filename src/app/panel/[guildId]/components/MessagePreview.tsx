d5730018479b63f51fce8fc26af0d6d96fe23f7c
fabrisdev <ezepuntocom@hotmail.com>
2026-01-24 03:40:11 -0300

    Message preview working

--- src/app/panel/[guildId]/components/MessagePreview.tsx
+++ src/app/panel/[guildId]/components/MessagePreview.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+
 type Props = {
   message: string;
 };
 
 export function MessagePreview({ message }: Props) {
-  return <p>{message}</p>;
+  return (
+    <div className="bg-[#1a1a1e] p-2 rounded-md flex gap-4">
+      <Image
+        src="/favicon.ico"
+        alt="Mita bot's avatar"
+        height={48}
+        width={48}
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-2">
+          <p>Mita</p>
+          <small className="bg-[#5865f2] font-bold rounded-md flex justify-center items-center pl-1 pr-1">
+            APP
+          </small>
+        </div>
+        <p className="text-gray-200">{message}</p>
+      </div>
+    </div>
+  );
 }
