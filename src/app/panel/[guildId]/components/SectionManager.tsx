b558d6a9d854790cad319d34f41bddbaf33edcaa
fabrisdev <ezepuntocom@hotmail.com>
2026-01-24 04:18:45 -0300

    render all sections

--- src/app/panel/[guildId]/components/SectionManager.tsx
+++ src/app/panel/[guildId]/components/SectionManager.tsx
@@ -2,6 +2,10 @@
 
 import { useSectionStore } from "@/stores/section";
 import { ChannelsSection } from "../sections/ChannelsSection";
+import { CustomCommandsSection } from "../sections/CustomCommandsSection";
+import { ModerationSection } from "../sections/ModerationSection";
+import { TicketLogSection } from "../sections/TicketLogSection";
+import { WelcomeSection } from "../sections/WelcomeSection";
 import { SectionWrapper } from "./SectionWrapper";
 
 type Props = {
@@ -12,7 +16,17 @@ export function SectionManager({ jwt }: Props) {
   const selected = useSectionStore((state) => state.section);
   return (
     <SectionWrapper>
-      {selected === "Channels" ? <ChannelsSection jwt={jwt} /> : null}
+      {selected === "Channels" ? (
+        <ChannelsSection jwt={jwt} />
+      ) : selected === "Moderation" ? (
+        <ModerationSection />
+      ) : selected === "Custom commands" ? (
+        <CustomCommandsSection />
+      ) : selected === "Ticket log" ? (
+        <TicketLogSection />
+      ) : (
+        <WelcomeSection />
+      )}
     </SectionWrapper>
   );
 }
