f66ba0c2cfbbc993ec3fb58fe923bcc73bf4307d
fabrisdev <ezepuntocom@hotmail.com>
2026-01-24 03:11:32 -0300

    better styles

--- src/app/panel/[guildId]/components/SectionOption.tsx
+++ src/app/panel/[guildId]/components/SectionOption.tsx
@@ -6,9 +6,14 @@ type Props = {
 
 export function SectionOption({ name }: Props) {
   const setSection = useSectionStore((state) => state.setSection);
+  const isSelected = useSectionStore((state) => state.section) === name;
   return (
     <li>
-      <button type="button" onClick={() => setSection(name)}>
+      <button
+        type="button"
+        onClick={() => setSection(name)}
+        className={`cursor-pointer w-full rounded-md bg-purple-500 p-2 border-2 hover:bg-purple-600  ${isSelected && "bg-red-400 hover:bg-red-500 font-bold"}`}
+      >
         {name}
       </button>
     </li>
