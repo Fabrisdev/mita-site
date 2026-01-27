516db1a90d7081ef97b087002894a66940fdb789
fabrisdev <ezepuntocom@hotmail.com>
2026-01-27 01:55:03 -0300

    better position

--- src/app/panel/[guildId]/components/SectionOption.tsx
+++ src/app/panel/[guildId]/components/SectionOption.tsx
@@ -11,12 +11,12 @@ export function SectionOption({ name }: Props) {
   return (
     <li className="flex gap-2 justify-center items-center group relative">
       <div
-        className={`bg-white h-[0%] opacity-0 w-2 rounded-xl group-hover:opacity-100 group-hover:h-[50%] transition-all duration-150 ${isSelected && "h-full group-hover:h-full opacity-100"}`}
+        className={`bg-white h-[0%] opacity-0 w-2 rounded-xl group-hover:opacity-100 group-hover:h-[50%] transition-all duration-150 absolute right-full mr-1 ${isSelected && "h-full group-hover:h-full opacity-100"}`}
       ></div>
       <button
         type="button"
         onClick={() => setSection(name)}
-        className="cursor-pointer w-full rounded-xl bg-[#1a1a1e] border border-[#28282c] hover:bg-[#242428] p-2 peer"
+        className="cursor-pointer w-full rounded-xl bg-[#1a1a1e] border border-[#28282c] hover:bg-[#242428] p-2 peer ml-1"
       >
         <Image src={`/${name}.svg`} width={32} height={32} alt={name} />
       </button>
