f58a08116a8c1b2810eafae0bd84fdd344376574
fabrisdev <ezepuntocom@hotmail.com>
2026-01-26 23:20:11 -0300

    added left line

--- src/app/panel/[guildId]/components/SectionOption.tsx
+++ src/app/panel/[guildId]/components/SectionOption.tsx
@@ -9,11 +9,14 @@ export function SectionOption({ name }: Props) {
   const setSection = useSectionStore((state) => state.setSection);
   const isSelected = useSectionStore((state) => state.section) === name;
   return (
-    <li>
+    <li className="flex gap-2 justify-center items-center group">
+      <div
+        className={`bg-white h-[0%] w-2 rounded-xl group-hover:h-[50%] transition-all duration-150 ${isSelected && "h-full group-hover:h-full"}`}
+      ></div>
       <button
         type="button"
         onClick={() => setSection(name)}
-        className="cursor-pointer w-full rounded-xl bg-gray-800 border border-gray-700 hover:bg-gray-900 p-2"
+        className="cursor-pointer w-full rounded-xl bg-gray-800 border border-gray-700 hover:bg-gray-900 p-2 peer"
       >
         <Image src={`/${name}.svg`} width={32} height={32} alt={name} />
       </button>
