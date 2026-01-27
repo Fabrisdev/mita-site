f1adb6b5f8e9498920a0bd851de9038ce1747763
fabrisdev <ezepuntocom@hotmail.com>
2026-01-27 04:17:01 -0300

    better ux

--- src/app/panel/[guildId]/components/MessageInput.tsx
+++ src/app/panel/[guildId]/components/MessageInput.tsx
@@ -3,6 +3,7 @@ import { MessagePreview } from "./MessagePreview";
 
 export function MessageInput() {
   const [message, setMessage] = useState("Hello everyone!");
+  const isEmpty = message.trim().length === 0;
   return (
     <>
       <input
@@ -11,8 +12,9 @@ export function MessageInput() {
         placeholder="Message"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
-        className="border border-[#28282c] p-2 rounded-md"
+        className={`border border-[#28282c] p-2 rounded-md outline-none focus:outline-none focus:ring-0 ${isEmpty ? "border-red-500" : "border-[#28282c]"}`}
       />
+      {isEmpty && <small>⚠️ Message cannot be empty.</small>}
       <MessagePreview message={message} />
     </>
   );
