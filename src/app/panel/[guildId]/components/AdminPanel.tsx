1808cad910dee1c36644df279dd8e8b0ad1a57f6
fabrisdev <ezepuntocom@hotmail.com>
2026-01-26 22:18:41 -0300

    react query on bot status

--- src/app/panel/[guildId]/components/AdminPanel.tsx
+++ src/app/panel/[guildId]/components/AdminPanel.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LoggedInAsPopup } from "@/auth/components/LoggedInAsPopup";
 import { BotStatus } from "./BotStatus";
 import { Header } from "./Header";
@@ -11,22 +14,26 @@ type Props = {
   jwt: string;
 };
 
+const queryClient = new QueryClient();
+
 export function AdminPanel({ username, id, avatar, jwt }: Props) {
   return (
     <div className="bg-purple-950 min-h-svh flex flex-col">
-      <Header>
-        <BotStatus />
-        <LoggedInAsPopup
-          username={username}
-          id={id}
-          avatar={avatar}
-          disableDefaultPositioning
-        />
-      </Header>
-      <div className="flex flex-1">
-        <Sidebar />
-        <SectionManager jwt={jwt} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <Header>
+          <BotStatus />
+          <LoggedInAsPopup
+            username={username}
+            id={id}
+            avatar={avatar}
+            disableDefaultPositioning
+          />
+        </Header>
+        <div className="flex flex-1">
+          <Sidebar />
+          <SectionManager jwt={jwt} />
+        </div>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/app/panel/[guildId]/components/BotStatus.tsx
+++ src/app/panel/[guildId]/components/BotStatus.tsx
@@ -1,33 +1,37 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { BotService } from "@/bot-service";
 
 export function BotStatus() {
-  const [isOnline, setIsOnline] = useState(false);
-  const [lastCheckedAt, setLastCheckedAt] = useState<Date | null>(null);
-
-  useEffect(() => {
-    BotService.isOnline()
-      .then(() => setIsOnline(true))
-      .catch(() => setIsOnline(false))
-      .finally(() => setLastCheckedAt(new Date()));
-  }, []);
-
-  const formattedTime = lastCheckedAt?.toLocaleTimeString("en-US", {
-    hour12: true,
-    hour: "numeric",
-    minute: "2-digit",
-    second: "2-digit",
+  const {
+    data: isOnline,
+    isFetching,
+    dataUpdatedAt,
+  } = useQuery({
+    queryKey: ["bot-status"],
+    queryFn: BotService.isOnline,
+    refetchInterval: 100_000,
   });
+
+  const formattedTime = dataUpdatedAt
+    ? new Date(dataUpdatedAt)?.toLocaleTimeString("en-US", {
+        hour12: true,
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    : null;
+  const message = isFetching ? "Checking..." : isOnline ? "ONLINE" : "OFFLINE";
+
   return (
     <div className="flex justify-center items-center gap-2">
       <span
         className={`font-bold p-2 rounded-xl border-2 ${isOnline ? "bg-green-400" : "bg-red-400"}`}
       >
-        {isOnline ? "ONLINE" : "OFFLINE"}
+        {message}
       </span>
-      <i className="text-">(last checked at: {formattedTime})</i>
+      {formattedTime && <i>(last checked at: {formattedTime})</i>}
     </div>
   );
 }
