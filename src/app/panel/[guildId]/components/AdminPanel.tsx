89ffaadca67c2b63fe567277986a1316efe3dbb0
fabrisdev <ezepuntocom@hotmail.com>
2026-01-26 23:30:30 -0300

    remove logged in as popup

--- src/app/panel/[guildId]/components/AdminPanel.tsx
+++ src/app/panel/[guildId]/components/AdminPanel.tsx
@@ -1,33 +1,23 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { LoggedInAsPopup } from "@/auth/components/LoggedInAsPopup";
 import { BotStatus } from "./BotStatus";
 import { Header } from "./Header";
 import { SectionManager } from "./SectionManager";
 import { Sidebar } from "./Sidebar";
 
 type Props = {
-  username: string;
-  id: string;
-  avatar: string;
   jwt: string;
 };
 
 const queryClient = new QueryClient();
 
-export function AdminPanel({ username, id, avatar, jwt }: Props) {
+export function AdminPanel({ jwt }: Props) {
   return (
     <div className="bg-[#1a1a1e] min-h-svh flex flex-col">
       <QueryClientProvider client={queryClient}>
         <Header>
           <BotStatus />
-          <LoggedInAsPopup
-            username={username}
-            id={id}
-            avatar={avatar}
-            disableDefaultPositioning
-          />
         </Header>
         <div className="flex flex-1">
           <Sidebar />
--- src/app/panel/[guildId]/page.tsx
+++ src/app/panel/[guildId]/page.tsx
@@ -1,23 +1,11 @@
-import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { AdminPanel } from "./components/AdminPanel";
-
-const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export default async function AdminPanelPage() {
   const cookieStore = await cookies();
   const session = cookieStore.get("session");
   if (!session) return redirect("/");
-  const verifyResult = await jwtVerify(session.value, secret);
-  const { payload } = verifyResult;
-  const { avatar, id, username } = payload;
-  return (
-    <AdminPanel
-      id={id as string}
-      username={username as string}
-      avatar={avatar as string}
-      jwt={session.value}
-    />
-  );
+
+  return <AdminPanel jwt={session.value} />;
 }
