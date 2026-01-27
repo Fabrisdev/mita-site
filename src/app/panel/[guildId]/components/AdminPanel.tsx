1089b11d6d4f22df471ff218109155fbda44de0a
fabrisdev <ezepuntocom@hotmail.com>
2026-01-27 02:24:27 -0300

    better bot status ui

--- src/app/panel/[guildId]/components/AdminPanel.tsx
+++ src/app/panel/[guildId]/components/AdminPanel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useSectionStore } from "@/stores/section";
 import { BotStatus } from "./BotStatus";
 import { Header } from "./Header";
 import { SectionManager } from "./SectionManager";
@@ -13,11 +14,13 @@ type Props = {
 const queryClient = new QueryClient();
 
 export function AdminPanel({ jwt }: Props) {
+  const section = useSectionStore((state) => state.section);
   return (
     <div className="bg-[#1a1a1e] min-h-svh flex flex-col">
       <QueryClientProvider client={queryClient}>
         <Header>
           <BotStatus />
+          <p>{section}</p>
         </Header>
         <div className="flex flex-1">
           <Sidebar />
--- src/app/panel/[guildId]/components/BotStatus.tsx
+++ src/app/panel/[guildId]/components/BotStatus.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useBotStatus } from "../hooks/useBotStatus";
 
 export function BotStatus() {
@@ -7,16 +8,27 @@ export function BotStatus() {
 
   const formattedTime = formatTime(updatedAt);
   const message = isFetching ? "Checking..." : isOnline ? "ONLINE" : "OFFLINE";
+  const lastCheckedAtMessage = isFetched
+    ? `Last checked at: ${formattedTime}`
+    : "Haven't checked yet";
 
   return (
-    <div className="flex justify-center items-center gap-2">
-      <span
-        className={`font-bold p-2 rounded-xl border-2 ${isOnline ? "bg-green-400" : "bg-red-400"}`}
-      >
-        {message}
-      </span>
-      {isFetched && <i>(last checked at: {formattedTime})</i>}
-    </div>
+    <li className="flex gap-2 justify-center items-center group relative">
+      <div
+        className={`bg-white h-[0%] opacity-0 w-2 rounded-xl group-hover:opacity-100 group-hover:h-[50%] transition-all duration-150 absolute right-full mr-1`}
+      ></div>
+      <div className="cursor-pointer w-full rounded-xl bg-[#1a1a1e] border border-[#28282c] hover:bg-[#242428] p-2 peer ml-1">
+        <Image
+          src={`/status.svg`}
+          width={32}
+          height={32}
+          alt={`Bot status: ${message}. Last checked at: ${formattedTime}`}
+        />
+      </div>
+      <p className="opacity-0 text-[#efeff1] font-bold group-hover:opacity-100 bg-[#242429] absolute left-full p-2 rounded-md ml-4 whitespace-nowrap transition border border-[#2e2e33] before:content-[''] before:absolute before:-left-1.5 before:top-1/2 before:-translate-y-1/2 before:border-y-6 before:border-y-transparent before:border-r-6 before:border-r-[#242429]">
+        {lastCheckedAtMessage}
+      </p>
+    </li>
   );
 }
 
