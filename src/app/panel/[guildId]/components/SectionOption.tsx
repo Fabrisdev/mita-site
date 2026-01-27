9f585930aa7451d5231edcc98788a4e0f738712a
fabrisdev <ezepuntocom@hotmail.com>
2026-01-27 00:58:38 -0300

    Added bubble

--- src/app/panel/[guildId]/components/SectionOption.tsx
+++ src/app/panel/[guildId]/components/SectionOption.tsx
@@ -9,7 +9,7 @@ export function SectionOption({ name }: Props) {
   const setSection = useSectionStore((state) => state.setSection);
   const isSelected = useSectionStore((state) => state.section) === name;
   return (
-    <li className="flex gap-2 justify-center items-center group">
+    <li className="flex gap-2 justify-center items-center group relative">
       <div
         className={`bg-white h-[0%] opacity-0 w-2 rounded-xl group-hover:opacity-100 group-hover:h-[50%] transition-all duration-150 ${isSelected && "h-full group-hover:h-full opacity-100"}`}
       ></div>
@@ -20,6 +20,9 @@ export function SectionOption({ name }: Props) {
       >
         <Image src={`/${name}.svg`} width={32} height={32} alt={name} />
       </button>
+      <p className="opacity-0 text-[#efeff1] font-bold group-hover:opacity-100 bg-[#242429] absolute left-full p-2 rounded-md ml-4 whitespace-nowrap transition border border-[#2e2e33] before:content-[''] before:absolute before:-left-1.5 before:top-1/2 before:-translate-y-1/2 before:border-y-6 before:border-y-transparent before:border-r-6 before:border-r-[#242429]">
+        {name}
+      </p>
     </li>
   );
 }
