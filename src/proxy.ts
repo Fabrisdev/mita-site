61dfd4d19fb7df351b26d392553898be914fbec5
fabrisdev <ezepuntocom@hotmail.com>
2026-01-26 21:19:17 -0300

    Added check for service down when going to admin panel

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,7 @@
 import { jwtVerify } from "jose";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { BOT_API_URL } from "./consts";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
@@ -13,7 +14,12 @@ export async function proxy(req: NextRequest) {
 
   try {
     await jwtVerify(session.value, secret);
-
+    const online = await fetch(`${BOT_API_URL}/status/ok`)
+      .then((res) => res.ok)
+      .catch(() => false);
+    console.log(online);
+    if (!online)
+      return Response.redirect(new URL("/?error=service_down", req.url));
     return NextResponse.next();
   } catch {
     return NextResponse.redirect(new URL("/", req.url));
