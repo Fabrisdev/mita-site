6d0d7ea6d4cfb1341868aa06a0358d0eccc8588e
fabrisdev <ezepuntocom@hotmail.com>
2026-01-28 21:32:40 -0300

    better colors

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -13,13 +13,13 @@ export default async function ChooseServerPage() {
     (_, i) => _guilds[i % _guilds.length],
   );
   return (
-    <div className="p-10 flex flex-col gap-5">
+    <div className="p-10 flex flex-col gap-5 bg-[#121214] min-h-svh">
       <h1 className="text-3xl text-center font-bold">Choose a server</h1>
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] justify-items-center gap-6">
         {guilds.map((guild) => (
           <li
             key={guild.id}
-            className="flex justify-center items-center gap-5 flex-col bg-gray-600 min-w-60 p-4 rounded-md"
+            className="flex justify-center items-center gap-5 flex-col bg-[#1a1a1e] min-w-60 p-4 rounded-md border border-[#2e2e33]"
           >
             <img
               src={guild.icon}
@@ -31,7 +31,7 @@ export default async function ChooseServerPage() {
             <p>{guild.name}</p>
             <Link
               href={`/panel/${guild.id}`}
-              className="bg-gray-800 w-full text-center rounded-md p-2 font-bold hover:bg-gray-900 transition"
+              className="bg-[#121214] border border-[#2e2e33] w-full text-center rounded-md p-2 font-bold hover:bg-[#242428] transition"
             >
               Go!
             </Link>
