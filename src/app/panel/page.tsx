356d5cb865583a8f98246dfa4413421b80d8bcb1
fabrisdev <ezepuntocom@hotmail.com>
2026-01-28 23:06:51 -0300

    simple header

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -23,7 +23,10 @@ export default async function ChooseServerPage() {
     id: crypto.randomUUID(),
   }));
   return (
-    <div className="p-10 flex flex-col gap-5 bg-[#121214] min-h-svh">
+    <div className="flex flex-col gap-5 bg-[#121214] min-h-svh">
+      <header className="bg-[#1a1a1e] flex justify-center items-center p-2 w-full border-b border-[#2e2e33]">
+        <p>Server selector</p>
+      </header>
       <h1 className="text-3xl text-center font-bold">Choose a server</h1>
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] justify-items-center gap-6">
         {guilds.map((guild, i) => (
