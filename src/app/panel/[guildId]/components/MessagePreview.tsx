53f4f4c7c6387995f166615f975ac31ed7880e99
fabrisdev <ezepuntocom@hotmail.com>
2026-02-02 00:12:16 -0300

    displaying time

--- src/app/panel/[guildId]/components/MessagePreview.tsx
+++ src/app/panel/[guildId]/components/MessagePreview.tsx
@@ -36,7 +36,11 @@ export function MessagePreview({ message, user }: Props) {
       <div className="flex flex-col">
         <div className="flex gap-2">
           <p>{user ? user.name : "Mita"}</p>
-          {!user && (
+          {user ? (
+            <small className="flex justify-center items-center text-[#7a7b83]">
+              {discordLikeTimestamp(user.sentAt)}
+            </small>
+          ) : (
             <small className="bg-[#5865f2] font-bold rounded-md flex justify-center items-center pl-1 pr-1 h-min">
               APP
             </small>
@@ -46,4 +50,38 @@ export function MessagePreview({ message, user }: Props) {
       </div>
     </div>
   );
+}
+
+function discordLikeTimestamp(unix: number) {
+  const date = new Date(unix);
+  const now = new Date();
+
+  const isSameDay = (a: Date, b: Date) =>
+    a.getFullYear() === b.getFullYear() &&
+    a.getMonth() === b.getMonth() &&
+    a.getDate() === b.getDate();
+
+  const yesterday = new Date(now);
+  yesterday.setDate(now.getDate() - 1);
+
+  const time = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  if (isSameDay(date, now)) {
+    return `Today at ${time}`;
+  }
+
+  if (isSameDay(date, yesterday)) {
+    return `Yesterday at ${time}`;
+  }
+
+  const fullDate = date.toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  return `${fullDate} ${time}`;
 }
