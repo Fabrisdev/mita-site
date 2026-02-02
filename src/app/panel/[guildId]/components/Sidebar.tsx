d8c40aa7f7c7b0348bfa9d297503794f2a4eb248
fabrisdev <ezepuntocom@hotmail.com>
2026-02-02 20:33:51 -0300

    fix scroll

--- src/app/panel/[guildId]/components/Sidebar.tsx
+++ src/app/panel/[guildId]/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { BotStatus } from "./BotStatus";
 import { GoBack } from "./GoBack";
 import { SectionOption } from "./SectionOption";
@@ -9,8 +10,26 @@ type Props = {
 };
 
 export function Sidebar({ jwt }: Props) {
+  const ref = useRef<HTMLUListElement>(null);
+  useEffect(() => {
+    function onScroll() {
+      if (ref.current === null) return;
+      const pixels = 40 - window.scrollY;
+      ref.current.style.height =
+        pixels < 0 ? "100svh" : `calc(100svh - ${pixels}px)`;
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  });
+
   return (
-    <ul className="bg-[#121214] flex flex-col gap-2 p-2">
+    <ul
+      ref={ref}
+      className="bg-[#121214] sticky top-0 h-[calc(100svh-40px)] flex flex-col gap-2 p-2"
+    >
       <SectionOption name="Moderation" />
       <SectionOption name="Channels" />
       <SectionOption name="Welcome & greeting" />
