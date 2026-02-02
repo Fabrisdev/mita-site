769cfdc5f2edaf92c5b5af022078ec68202a556d
fabrisdev <ezepuntocom@hotmail.com>
2026-02-01 22:13:44 -0300

    rendering ticket

--- src/app/panel/[guildId]/sections/TicketLogSection.tsx
+++ src/app/panel/[guildId]/sections/TicketLogSection.tsx
@@ -8,14 +8,25 @@ type Props = {
 
 export function TicketLogSection({ jwt }: Props) {
   const params = useParams<{ guildId: string }>();
-  const tickets = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["tickets"],
     queryFn: () =>
       BotService.Tickets.all({ session: jwt, guildId: params.guildId }),
   });
+  if (isLoading) return <p>Loading...</p>;
+  if (!data || data.length === 0) return <p>Nobody has made a ticket yet.</p>;
   return (
     <div>
       <p>Tickets</p>
+      <div>
+        {data.map((ticket) => (
+          <ul key={ticket._id}>
+            <p>ID: {ticket._id}</p>
+            <p>Opened by: {ticket.ownerId}</p>
+            <p>Status: {ticket.status}</p>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
