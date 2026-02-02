05cb0d432eeca7346cdda0302955d32fa48e1cd5
fabrisdev <ezepuntocom@hotmail.com>
2026-02-02 00:18:13 -0300

    better styles

--- src/app/panel/[guildId]/sections/TicketLogSection.tsx
+++ src/app/panel/[guildId]/sections/TicketLogSection.tsx
@@ -16,28 +16,35 @@ export function TicketLogSection({ jwt }: Props) {
   });
   if (isLoading) return <p>Loading...</p>;
   if (!data || data.length === 0) return <p>Nobody has made a ticket yet.</p>;
-  return data.map((ticket) => (
-    <div key={ticket._id} className="border p-2 rounded-md">
-      <ul className="list-disc list-inside">
-        <li>
-          <strong>ID</strong>: {ticket._id}
-        </li>
-        <li>
-          <strong>Opened by</strong>: {ticket.ownerId}
-        </li>
-        <li>
-          <strong>Status</strong>: {ticket.status}
-        </li>
-      </ul>
-      <div>
-        {ticket.messages.toReversed().map((message) => (
-          <MessagePreview
-            key={message._id}
-            message={message.content}
-            user={{ ...message.user, sentAt: message.sentAt }}
-          />
-        ))}
-      </div>
+  return (
+    <div className="flex flex-col gap-2">
+      {data.map((ticket) => (
+        <div
+          key={ticket._id}
+          className="border p-2 rounded-md border-[#28282c] bg-[#121214]"
+        >
+          <ul className="list-disc list-inside">
+            <li>
+              <strong>ID</strong>: {ticket._id}
+            </li>
+            <li>
+              <strong>Opened by</strong>: {ticket.ownerId}
+            </li>
+            <li>
+              <strong>Status</strong>: {ticket.status}
+            </li>
+          </ul>
+          <div className="border rounded-md border-[#28282c]">
+            {ticket.messages.toReversed().map((message) => (
+              <MessagePreview
+                key={message._id}
+                message={message.content}
+                user={{ ...message.user, sentAt: message.sentAt }}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
-  ));
+  );
 }
