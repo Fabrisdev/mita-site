689badf103393986c009924d15b42fd08d93654c
fabrisdev <ezepuntocom@hotmail.com>
2026-02-02 23:00:06 -0300

    using exported function

--- src/app/panel/[guildId]/components/MessagePreview.tsx
+++ src/app/panel/[guildId]/components/MessagePreview.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { discordLikeTimestamp } from "@/app/general/helper";
 
 type Props = {
   message: string;
@@ -50,38 +51,4 @@ export function MessagePreview({ message, user }: Props) {
       </div>
     </div>
   );
-}
-
-function discordLikeTimestamp(unix: number) {
-  const date = new Date(unix);
-  const now = new Date();
-
-  const isSameDay = (a: Date, b: Date) =>
-    a.getFullYear() === b.getFullYear() &&
-    a.getMonth() === b.getMonth() &&
-    a.getDate() === b.getDate();
-
-  const yesterday = new Date(now);
-  yesterday.setDate(now.getDate() - 1);
-
-  const time = date.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  if (isSameDay(date, now)) {
-    return `Today at ${time}`;
-  }
-
-  if (isSameDay(date, yesterday)) {
-    return `Yesterday at ${time}`;
-  }
-
-  const fullDate = date.toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-
-  return `${fullDate} ${time}`;
 }
